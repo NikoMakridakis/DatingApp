1ce20dfb029b6b5b8e634e705e6b41bba43a7319
Niko Makridakis <nmak345@gmail.com>
2020-07-02 05:54:23 -0500

    reverted styling

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthService } from '../_services/auth.service';
-import { AlertifyService } from '../_services/alertify.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,39 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  model: any = {};
   registerMode = false;
 
-  constructor(
-    private http: HttpClient,
-    public authService: AuthService,
-    private alertify: AlertifyService,
-    private router: Router
-    ) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-  }
-
-  login() {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('Logged in successfully');
-    }, error => {
-    }, () => {
-      this.router.navigate(['/members']);
-    });
-  }
-
-  loggedIn() {
-    return this.authService.loggedIn();
-  }
-
-  logout() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    this.authService.decodedToken = null;
-    this.authService.currentUser = null;
-    this.alertify.message('logged out');
-    this.router.navigate(['/home']);
   }
 
   registerToggle() {
